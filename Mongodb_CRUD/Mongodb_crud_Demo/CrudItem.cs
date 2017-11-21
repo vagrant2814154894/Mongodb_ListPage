bf02a9dd58c7f5bd8c510f5af5ab7449d5b77710
vagrant2814154894 <2814154894@qq.com>
2017-11-21 16:28:31 +0800

    1

---
+++ Mongodb_CRUD/Mongodb_crud_Demo/CrudItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongodb_crud_Demo
+{
+    /// <summary>
+    /// CRUD表：tblCrud
+    /// </summary>
+    public class CrudItem
+    {
+    }
+}
---
+++ Mongodb_CRUD/Mongodb_crud_Demo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Mongodb_crud_Demo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var readPreference = new ReadPreference(ReadPreferenceMode.SecondaryPreferred);
+            MongoDatabaseSettings mdSetting = new MongoDatabaseSettings();
+            mdSetting.ReadPreference = readPreference;
+            var connectionString = "mongodb://127.0.0.1:27017";
+            var database = "Test";
+            MongoClient client = new MongoClient(connectionString);//连接mogodb数据库
+            IMongoDatabase db = client.GetDatabase(database, mdSetting);//数据库
+            var collection = db.GetCollection<CrudItem>("tblCrud");//表
+
+            var documents = new BsonDocument[]
+{
+    new BsonDocument
+    {
+        { "item", "journal" },
+        { "qty", 25 },
+        { "size", new BsonDocument { { "h", 14 }, { "w", 21 }, {  "uom", "cm"} } },
+        { "status", "A" }
+    },
+    new BsonDocument
+    {
+        { "item", "notebook" },
+        { "qty", 50 },
+        { "size", new BsonDocument { { "h",  8.5 }, { "w", 11 }, {  "uom", "in"} } },
+        { "status", "A" }
+    },
+    new BsonDocument
+    {
+        { "item", "paper" },
+        { "qty", 100 },
+        { "size", new BsonDocument { { "h",  8.5 }, { "w", 11 }, {  "uom", "in"} } },
+        { "status", "D" }
+    },
+    new BsonDocument
+    {
+        { "item", "planner" },
+        { "qty", 75 },
+        { "size", new BsonDocument { { "h", 22.85 }, { "w", 30  }, {  "uom", "cm"} } },
+        { "status", "D" }
+    },
+    new BsonDocument
+    {
+        { "item", "postcard" },
+        { "qty", 45 },
+        { "size", new BsonDocument { { "h", 10 }, { "w", 15.25 }, {  "uom", "cm"} } },
+        { "status", "A" }
+    },
+};
+            collection.InsertMany(documents);
+
+            //client = new MongoClient(conn);//连接mogodb数据库
+
+            //if (client == null)
+            //{
+            //    Console.WriteLine("mongodb数据库连接失败");
+            //}
+
+            //database = client.GetDatabase(dbName);//数据库
+            //var collection = database.GetCollection<CrudItem>("tblCard");//表
+        }
+    }
+}
